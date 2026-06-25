f1aff13ce83dd4f3faa1b4a5f73392b57aa76a12
Jader Tenguam <jader.java.contato@gmail.com>
2026-06-24 22:08:41 -0300

    configurando infraestrutura Spring Boot

---
+++ backend/credit-engine/src/test/java/br/com/srm/credit/bootstrap/CreditEngineApplicationIT.java
@@ -0,0 +1,40 @@
+package br.com.srm.credit.bootstrap;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class CreditEngineApplicationIT {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Container
+    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withDatabaseName("credit_assignment_db")
+            .withUsername("platform_user")
+            .withPassword("platform_password");
+
+    @DynamicPropertySource
+    static void registerDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+        registry.add("spring.datasource.driver-class-name", POSTGRES::getDriverClassName);
+    }
+
+    @Test
+    void applicationContextStartsWithPostgresRunningInDocker() {
+        assertNotNull(applicationContext);
+    }
+}
